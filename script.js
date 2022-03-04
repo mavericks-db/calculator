6bdd6f2dfa48243c47f94932c56735893721dd91
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-03-04 18:57:37 +0500

    session

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
 let clear = document.getElementById('clr');
 let backspace = document.getElementById('backspace');
-let keypad7 = document.getElementById('keypad7');
-let keypad8 = document.getElementById('keypad8');
-let keypad9 = document.getElementById('keypad9');
-let divide = document.getElementById('divide');
-let keypad4 = document.getElementById('keypad4');
-let keypad5 = document.getElementById('keypad5');
-let keypad6 = document.getElementById('keypad6');
-let multiply = document.getElementById('multiply');
-let keypad1 = document.getElementById('keypad1');
-let keypad2 = document.getElementById('keypad2');
-let keypad3 = document.getElementById('keypad3');
-let minus = document.getElementById('minus');
 let point = document.getElementById('point');
-let keypad0 = document.getElementById('keypad0');
 let equal = document.getElementById('equal');
-let plus = document.getElementById('plus');
 let firstDisplay = document.getElementById('firstDisplay')
 let secondDisplay = document.getElementById('secondDisplay')
+let keypadBtn = document.querySelectorAll('.keypadBtn');
+let operatorBtn = document.querySelectorAll('.operatorBtn');
+
+keypadBtn.forEach(button => button.addEventListener('click', digit));
+operatorBtn.forEach(button => button.addEventListener('click', operation));
 
 clear.addEventListener('click', cls);
 backspace.addEventListener('click', removeItem);
-keypad7.addEventListener('click', digit);
-keypad8.addEventListener('click', digit);
-keypad9.addEventListener('click', digit);
-divide.addEventListener('click', operation);
-keypad4.addEventListener('click', digit);
-keypad5.addEventListener('click', digit);
-keypad6.addEventListener('click', digit);
-multiply.addEventListener('click', operation);
-keypad1.addEventListener('click', digit);
-keypad2.addEventListener('click', digit);
-keypad3.addEventListener('click', digit);
-minus.addEventListener('click', operation);
 point.addEventListener('click', decimal);
-keypad0.addEventListener('click', digit);
 equal.addEventListener('click', checkResult);
-plus.addEventListener('click', operation);
 
 function cls(e) {
     location.reload();
 }
-
 
 function removeItem() {
     secondDisplay.innerText = secondDisplay.innerText.toString().slice(0, -1);
@@ -87,8 +63,12 @@ function checkResult() {
     if (currentOperator === null || resetCondition == true) {
         return
     }
+    if (currentOperator === '/' && secondDisplay.innerText === '0') {
+        alert("Undefined");
+        return
+    }
     storeNum2 = secondDisplay.innerText;
-    secondDisplay.innerText = operate(currentOperator, storeNum1, storeNum2);
+    secondDisplay.innerText = (operate(currentOperator, storeNum1, storeNum2)).toFixed(5);
     firstDisplay.innerText = `${storeNum1} ${currentOperator} ${storeNum2} =`;
     currentOperator = null;
 }
