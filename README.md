9b2002b207f2dcf93e467862d70df7c7c9f2bfcd
Mavericks Balitaan <balitaanmavericks@gmail.com>
2022-05-01 16:41:56 +0500

    Update README file

--- README.md
+++ README.md
@@ -1,5 +1,96 @@
-# calculator
+![](https://img.shields.io/badge/The_Odin_Project-e3b261)
 
-## The Odin Project : Calculator
+# Calculator
 
-[Live Demo: ](https://mavericks-db.github.io/calculator/):point_left:
+> This is an on-screen calculator using HTML, CSS and JavaScript.
+
+
+## Built With
+
+- [x] Languages: HTML, Sass and JavaScript
+- [x] Linters: Lighthouse, Webhint, Stylelint and ESlint
+- [x] Code Editor: VS Code
+
+## Website Mockup 📱 💻 🖥️
+![all-devices-black](https://user-images.githubusercontent.com/98527559/166143599-c4f25310-0a15-4f52-903b-a1bc8e18e3d0.png)
+
+
+## Screenshots 📸
+Screenshots can be found in the repository.
+
+## Live Demo 🔗
+
+Check the live demo [here](https://mavericks-db.github.io/calculator/)👈
+
+## Getting Started
+
+To get a local copy up and running follow these simple steps.
+
+### Prerequisites
+
+- [x] A web browser like Google Chrome.
+- [x] A code editor like Visual Studio Code with Git and Node.js.
+
+You can check if Git is installed by running the following command in the terminal.
+```
+$ git --version
+```
+
+Likewise for Node.js and npm for package installation.
+```
+$ node --version && npm --version
+```
+
+### Setup
+
+Clone the repository using the GitHub link provided below.
+
+### Install
+
+In the terminal, go to your file directory and run this command.
+
+```
+$ git clone git@github.com:mavericks-db/calculator.git
+```
+
+### Usage
+
+Kindly modify the files as needed.
+
+### Run tests
+
+To check for linters, use the files provided by [Microverse](https://github.com/microverseinc/linters-config). A GitHub action is also set to run during pull request.
+```
+$ npm install
+```
+
+### Deployment
+
+This app is deployed in the GitHub Pages for easy viewing upon merged on the main branch.
+Please find the link in the Live Demo section.
+
+
+## Authors
+
+👤 **Mavericks Balitaan** <img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="20"/>
+
+  [![GitHub Badge](https://img.shields.io/badge/-mavericks--db-white?logo=GitHub&logoColor=181717&style=plastic)](https://github.com/mavericks-db) [![Twitter Badge](https://img.shields.io/badge/-mavericks__db-white?logo=Twitter&logoColor=1DA1F2&style=plastic)](https://twitter.com/mavericks_db) [![Facebook Badge](https://img.shields.io/badge/-mavericksdb-white?logo=Facebook&logoColor=1877F2&style=plastic)](https://www.facebook.com/mavericksdb/) [![LinkedIn Badge](https://img.shields.io/badge/-mavericks--db-white?logo=LinkedIn&logoColor=0A66C2&style=plastic)](https://www.linkedin.com/in/mavericks-db/) [![Gmail Badge](https://img.shields.io/badge/-@balitaanmavericks-white?logo=Gmail&logoColor=EA4335&style=plastic)](mailto:balitaanmavericks@gmail.com)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](https://github.com/mavericks-db/calculator/issues).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- [The Odin Project](https://www.theodinproject.com/)
+- [Microverse](https://www.microverse.org/)
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
--- script.js
+++ script.js
@@ -1,139 +1,142 @@
-let clear = document.getElementById('clr');
-let backspace = document.getElementById('backspace');
-let point = document.getElementById('point');
-let equal = document.getElementById('equal');
-let firstDisplay = document.getElementById('firstDisplay')
-let secondDisplay = document.getElementById('secondDisplay')
-let keypadBtn = document.querySelectorAll('.keypadBtn');
-let operatorBtn = document.querySelectorAll('.operatorBtn');
+const clear = document.getElementById('clr');
+const backspace = document.getElementById('backspace');
+const point = document.getElementById('point');
+const equal = document.getElementById('equal');
+const firstDisplay = document.getElementById('firstDisplay');
+const secondDisplay = document.getElementById('secondDisplay');
+const keypadBtn = document.querySelectorAll('.keypadBtn');
+const operatorBtn = document.querySelectorAll('.operatorBtn');
 
-keypadBtn.forEach(button => button.addEventListener('click', digit));
-operatorBtn.forEach(button => button.addEventListener('click', operation));
+function cls() {
+  document.location.reload();
+}
+
+function removeItem() {
+  secondDisplay.innerText = secondDisplay.innerText.toString().slice(0, -1);
+}
+
+let resetCondition = false;
+function digit(e) {
+  if (secondDisplay.innerText === '0' || resetCondition === true) {
+    secondDisplay.innerText = '';
+    resetCondition = false;
+  }
+  secondDisplay.innerText += e.target.dataset.value;
+}
+
+function decimal(e) {
+  if (resetCondition === true) {
+    secondDisplay.innerText = '';
+    resetCondition = false;
+  }
+  if (secondDisplay.innerText === '') {
+    secondDisplay.innerText = '0';
+  }
+  if (secondDisplay.innerText.includes('.')) {
+    return;
+  }
+  secondDisplay.innerText += e.target.dataset.value;
+}
+let currentOperator = null;
+let storeNum1 = '';
+let storeNum2 = '';
+
+let result = 0;
+
+function add(a, b) {
+  return a + b;
+}
+
+function sub(a, b) {
+  return a - b;
+}
+
+function mul(a, b) {
+  return a * b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  a = Number(storeNum1);
+  b = Number(storeNum2);
+  operator = currentOperator;
+  switch (operator) {
+    case '/':
+      result = div(a, b);
+      break;
+    case '*':
+      result = mul(a, b);
+      break;
+    case '+':
+      result = add(a, b);
+      break;
+    case '-':
+      result = sub(a, b);
+      break;
+    default:
+      return null;
+  }
+  return result;
+}
+
+function checkResult() {
+  if (currentOperator === null || resetCondition === true) {
+    return;
+  }
+  if (currentOperator === '/' && secondDisplay.innerText === '0') {
+    alert('Undefined');
+    return;
+  }
+  storeNum2 = secondDisplay.innerText;
+
+  secondDisplay.innerText = (operate(currentOperator, storeNum1, storeNum2)).toFixed(5);
+  firstDisplay.innerText = `${storeNum1} ${currentOperator} ${storeNum2} =`;
+  currentOperator = null;
+}
+
+function operation(e) {
+  if (currentOperator !== null) {
+    checkResult();
+  }
+  storeNum1 = secondDisplay.innerText;
+  currentOperator = e.target.dataset.value;
+  firstDisplay.innerText = `${storeNum1} ${currentOperator}`;
+  resetCondition = true;
+}
+
+function keyboardInput(e) {
+  e.target.dataset.value = e.key;
+  if (e.key >= 0 && e.key <= 9) {
+    digit(e);
+  }
+  if (e.key === '.') {
+    decimal(e);
+  }
+
+  if (e.key === '+' || e.key === '-' || e.key === '*' || e.key === '/') {
+    operation(e);
+  }
+  if (e.key === 'Backspace') {
+    removeItem(e);
+  }
+  if (e.key === 'Enter') {
+    checkResult(e);
+  }
+  if (e.key === 'Escape') {
+    cls(e);
+  }
+}
+
+// Added keyboard support
+window.addEventListener('keydown', keyboardInput);
+
+keypadBtn.forEach((button) => button.addEventListener('click', digit));
+operatorBtn.forEach((button) => button.addEventListener('click', operation));
 
 clear.addEventListener('click', cls);
 backspace.addEventListener('click', removeItem);
 point.addEventListener('click', decimal);
 equal.addEventListener('click', checkResult);
-
-function cls(e) {
-    location.reload();
-}
-
-function removeItem() {
-    secondDisplay.innerText = secondDisplay.innerText.toString().slice(0, -1);
-}
-
-let resetCondition = false;
-function digit(e) {
-    if (secondDisplay.innerText === '0' || resetCondition == true) {
-        secondDisplay.innerText = '';
-        resetCondition = false;
-    }
-    secondDisplay.innerText += e.target.dataset.value;
-}
-
-function decimal(e) {
-    if (resetCondition == true) {
-        secondDisplay.innerText = '';
-        resetCondition = false;
-    }
-    if (secondDisplay.innerText === '') {
-        secondDisplay.innerText = '0';
-    }
-    if (secondDisplay.innerText.includes('.')) {
-        return
-    }
-    secondDisplay.innerText += e.target.dataset.value;
-}
-
-let currentOperator = null;
-let storeNum1 = '';
-let storeNum2 = '';
-function operation(e) {
-    if (currentOperator !== null) {
-        checkResult();
-    }
-    storeNum1 = secondDisplay.innerText;
-    currentOperator = e.target.dataset.value;
-    firstDisplay.innerText = `${storeNum1} ${currentOperator}`;
-    resetCondition = true;
-}
-
-function checkResult() {
-    if (currentOperator === null || resetCondition == true) {
-        return
-    }
-    if (currentOperator === '/' && secondDisplay.innerText === '0') {
-        alert("Undefined");
-        return
-    }
-    storeNum2 = secondDisplay.innerText;
-    secondDisplay.innerText = (operate(currentOperator, storeNum1, storeNum2)).toFixed(5);
-    firstDisplay.innerText = `${storeNum1} ${currentOperator} ${storeNum2} =`;
-    currentOperator = null;
-}
-
-let result = 0;
-function operate(operator, a, b) {
-    a = Number(storeNum1);
-    b = Number(storeNum2);
-    operator = currentOperator;
-    switch (operator) {
-        case '/':
-            result = div(a, b);
-            break;
-        case '*':
-            result = mul(a, b);
-            break;
-        case '+':
-            result = add(a, b);
-            break;
-        case '-':
-            result = sub(a, b);
-            break;
-    }
-    return result;
-}
-
-function add(a, b) {
-    return a + b;
-}
-
-function sub(a, b) {
-    return a - b;
-}
-
-function mul(a, b) {
-    return a * b;
-}
-
-function div(a, b) {
-    return a / b;
-}
-
-
-// Added keyboard support
-window.addEventListener('keydown', keyboardInput);
-
-function keyboardInput(e) {
-    e.target.dataset.value = e.key;
-    if (e.key >= 0 && e.key <= 9) {
-        digit(e);
-    }
-    if (e.key == '.') {
-        decimal(e);
-    }
-
-    if (e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') {
-        operation(e);
-    }
-    if (e.key == 'Backspace') {
-        removeItem(e);
-    }
-    if (e.key == 'Enter') {
-        checkResult(e);
-    }
-    if (e.key == 'Escape') {
-        cls(e);
-    }
-}
